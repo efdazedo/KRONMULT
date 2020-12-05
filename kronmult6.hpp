9aef3a49824e5365814665c9c205bacfeef59036
D'Azevedo, Ed F <dazevedoef@ornl.gov>
2020-12-04 21:53:56 -0500

    use kronmultx

--- kronmult6.hpp
+++ kronmult6.hpp
@@ -4,7 +4,7 @@
 #include "kroncommon.hpp"
 
 #include "kgemm_nt.hpp"
-#include "kronmult5.hpp"
+#include "kronmultx.hpp"
 
 //  -------------------------------------------
 //  device function to evaluate
@@ -31,84 +31,11 @@ void kronmult6( int const n,
 //      X is (n^6 by nvec)
 // -----------------
 {
-    int const lda = (lda_in == 0) ? n : lda_in;
-    int const n2 = n*n;
-    int const n4 = n2*n2;
-    int const n5 = n*n4;
-    int const n6 = n2*n4;
-
-    int const ldX = n6;
-    int const ldW = n6;
-
-    auto X = [=] (int const i,
-                  int const j) -> T& {
-            return( X_[ indx2f(i,j,ldX) ] );
-    };
-
-    auto W = [=] (int const i,
-                  int const j) -> T& {
-            return( W_[ indx2f(i,j,ldW) ] );
-    };
-
-
-
-
-    for(int i=1; i <= nvec; i++) {
-            T *Xi_ = &( X(1, i) );
-            T *Wi_ = &( W(1, i) );
-            int const ldXi = n5;
-            int const ldWi = n5;
-            // ----------------------------
-            // Xi viewed as (n^5 by n) array
-            // Wi viewed as (n^5 by n) array
-            // ----------------------------
-
-            auto Xi = [=] (int const i,
-                           int const j) -> T& {
-                    return(  Xi_[ indx2f(i,j,ldXi) ] );
-            };
-
-            auto Wi = [=] (int const i,
-                           int const j) -> T& {
-                    return(  Wi_[ indx2f(i,j,ldWi) ] );
-            };
-
-            // --------------------------------------------------------
-            // Wi(1:n^5, 1:n) = Xi(1:n^5, 1:n) * transpose(A1(1:n,1:n))
-            // --------------------------------------------------------
-            int const mm = n5;
-            int const nn = n;
-            int const kk = n;
-            T const alpha = 1;
-            T const beta = 0;
-
-            T const * const  Ap = &(Xi(1,1));
-            T const * const  Bp = A1_;
-            T       * const  Cp = &(Wi(1,1));
-
-            int const ld1 = ldXi;
-            int const ld2 = lda;
-            int const ld3 = ldWi;
-
-            kgemm_nt( mm,nn,kk, 
-                      alpha, Ap, ld1,
-                             Bp, ld2,
-                      beta,  Cp, ld3 );
-    };
-
-    int const next_nvec = nvec * n;
-
-    // --------------------------------
-    // note now X_ is used as workspace
-    // --------------------------------
-    {
-    kronmult5( n, next_nvec, 
-               A2_, A3_, A4_, A5_, A6_,
-               W_,  Y_,   X_, lda );
-    }
-
+    int constexpr ndim = 6;
+    kronmultx<T,ndim>(
+                      n, nvec,
+                      A1_,A2_,A3_,A4_,A5_,A6_,
+                      X_,Y_,W_,lda_in );
 }
-
-
 
 #endif
