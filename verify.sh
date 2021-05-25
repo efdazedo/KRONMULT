28fdbce0e2df983ea2ac5697dd3ce9c6bc284e44
D'azevedo, Ed <dazevedoef@ornl.gov>
2021-05-25 17:00:27 -0400

    include test for kgemm_tt

--- verify.sh
+++ verify.sh
@@ -2,8 +2,23 @@ export OMP_NUM_THREADS=8
 export OMP_STACKSIZE=2G
 make clean;
 make -j  ;
+echo " === test_kgemm_nn_batched ==== "
 ./test_kgemm_nn_batched
+echo " === test_kgemm_nt_batched ==== "
 ./test_kgemm_nt_batched
+echo " === test_kgemm_tt_batched ==== "
+./test_kgemm_tt_batched
+
+echo " === test_kronmult6_batched ==== "
 ./test_kronmult6_batched
+
+
+echo " === test_kronmult6_pbatched ==== "
 ./test_kronmult6_pbatched
+
+
+echo " === test_kronmult6_xbatched ==== "
 ./test_kronmult6_xbatched
+
+echo " === test_kronmult6_vbatched ==== "
+./test_kronmult6_vbatched
